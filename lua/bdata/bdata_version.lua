13b58208c0c8b87b66902e587bc4a2db1b4ca12b
xbeastguyx <xbeastguyx@gmail.com>
2016-02-20 03:03:26 -0500

    Timer fix.

--- lua/bdata/bdata_version.lua
+++ lua/bdata/bdata_version.lua
@@ -3,13 +3,7 @@
 function BData_CheckVersion()
 	http.Fetch( "https://raw.githubusercontent.com/xbeastguyx/Script-Versions/master/BData.txt", function( body )
 		if tonumber( body ) != tonumber( BData.Version ) then
-			if CLIENT then
-				if LocalPlayer():IsAdmin() then
-					LocalPlayer():ChatPrint( "[BData] There has been an update! You are running version " .. BData.Version .. ", the latest version is " .. body .. "." )
-				end
-			else
-				print( "[BData] There has been an update! You are running version " .. BData.Version .. ", the latest version is " .. body .. "." )
-			end
+			print( "[BData] There has been an update! You are running version " .. BData.Version .. ", the latest version is " .. body .. "." )
 		end
 	end )
 end
